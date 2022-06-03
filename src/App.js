51c46da7b0e44929b755554165347688a4de4371
motishokri <motahare.shokri@yahoo.com>
2022-06-03 11:12:54 +0430

    Added The GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This Project was Coded by {""}
+        <a href="https://serene-payne-47c9ff.netlify.app/" target="_blank">
+          Motahare Shokri {""}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/motishokri/react-weather-app"
+          target="_blank"
+        >
+          Open-Sourced On GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
