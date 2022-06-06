f349ddd3e452eeec8bbce26b2a70e8d8cc510ef8
motishokri <motahare.shokri@yahoo.com>
2022-06-06 11:57:23 +0430

    Added boostrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This Project was Coded by {""}
-        <a
-          href="https://serene-payne-47c9ff.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Motahare Shokri {""}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/motishokri/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-Sourced On GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This Project was Coded by {""}
+          <a
+            href="https://serene-payne-47c9ff.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Motahare Shokri {""}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/motishokri/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-Sourced On GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
