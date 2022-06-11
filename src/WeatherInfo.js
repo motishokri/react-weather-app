82ad8886bb77475d0b6ff82076aabf94dbef71a5
motishokri <motahare.shokri@yahoo.com>
2022-06-11 17:49:58 +0430

    Use the correct icon URL

---
+++ src/WeatherInfo.js
@@ -0,0 +1,39 @@
+import React from "react";
+import FormatedDate from "./FormatedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormatedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src={props.data.iconUrl}
+              alt={props.data.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">
+                {Math.round(props.data.temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
