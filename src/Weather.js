4555aeb07f138f7705293a5a82039def8dcde9c2
motishokri <motahare.shokri@yahoo.com>
2022-06-13 21:23:06 +0430

    Edit Weather Forecast

--- src/Weather.js
+++ src/Weather.js
@@ -55,7 +55,7 @@ export default function Weather(props) {
               <input
                 type="submit"
                 value="Search"
-                className="btn btn-primary w-100"
+                className="btn btn-warning w-100"
               />
             </div>
           </div>
